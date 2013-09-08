05a30489ff34ea00c7fbb8d21331888c7d7faea6
Michael Pankov <constantius9@gmail.com>
2013-09-09 00:44:29 +0400

    Start implementation of intersperse

---
+++ ch03/Intersperse.hs
@@ -0,0 +1,35 @@
+-- file: ch03/Intersperse.hs
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import Test.QuickCheck
+import Test.HUnit
+
+intersperse :: a -> [[a]] -> [a]
+intersperse _ _ = []
+
+test_NoElements =
+    intersperse ',' []
+    @?= ""
+
+test_SingleElement =
+    intersperse ',' ["foo"]
+    @?= "foo"
+
+test_MultipleElements =
+    intersperse ',' ["foo","bar","baz","quux"]
+    @?= "foo,bar,baz,quux"
+
+main = defaultMain tests
+
+tests = [
+    testGroup "Intersperse" [
+        testCase "Intersperse works right with no elements"
+            test_NoElements,
+        testCase "Intersperse works right with single element"
+            test_SingleElement,
+        testCase "Intersperse works right with multiple elements"
+            test_MultipleElements
+        ]
+    ]
