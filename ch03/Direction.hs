903cea3af6d1a0172f886007b00b09706cec9349
Michael Pankov <constantius9@gmail.com>
2013-09-09 23:39:04 +0400

    Add the tests for direction

--- ch03/Direction.hs
+++ ch03/Direction.hs
@@ -1,7 +1,45 @@
+import Prelude hiding (Left, Right)
+
+import Test.Framework (defaultMain, testGroup)
+import Test.Framework.Providers.HUnit
+import Test.Framework.Providers.QuickCheck2 (testProperty)
+
+import Test.QuickCheck
+import Test.HUnit
+
 data Direction = Left
                | Right
                | Straight
+                 deriving (Show, Eq)
 
 data Point2D = Point2D { x :: Double
                        , y :: Double
                        } deriving (Show, Eq, Ord)
+
+direction :: Point2D -> Point2D -> Point2D -> Direction
+direction a b c = undefined
+
+test_Left =
+    direction (Point2D {x=0, y=0}) (Point2D {x=1, y=1}) (Point2D {x=2, y=3})
+    @?= Left
+
+test_Straight =
+    direction (Point2D {x=0, y=0}) (Point2D {x=1, y=1}) (Point2D {x=2, y=2})
+    @?= Straight
+
+test_Right =
+    direction (Point2D {x=0, y=0}) (Point2D {x=1, y=1}) (Point2D {x=2, y=1})
+    @?= Right
+
+main = defaultMain tests
+
+tests = [
+    testGroup "Direction" [
+        testCase "Direction works right for left turn"
+            test_Left,
+        testCase "Direction works right for straight line"
+            test_Straight,
+        testCase "Direction works right for right turn"
+            test_Right
+        ]
+    ]
