ad3dbee0ce0c4d89e7cdc02558d445aea8a13436
Michael Pankov <constantius9@gmail.com>
2013-09-09 23:57:16 +0400

    Implement incorrect direction function

--- ch03/Direction.hs
+++ ch03/Direction.hs
@@ -12,12 +12,26 @@ data Direction = Left
                | Straight
                  deriving (Show, Eq)
 
-data Point2D = Point2D { x :: Double
-                       , y :: Double
+data Point2D = Point2D { x :: Integer
+                       , y :: Integer
                        } deriving (Show, Eq, Ord)
 
 direction :: Point2D -> Point2D -> Point2D -> Direction
-direction a b c = undefined
+direction a b c =
+    let x1 = x a
+        x2 = x b
+        x3 = x c
+        y1 = y a
+        y2 = y b
+        y3 = y c
+        k  = ((fromIntegral y1) - l) / (fromIntegral x1) :: Double
+        l  = (fromIntegral y2) - k * (fromIntegral x2) :: Double
+        s  = k * (fromIntegral x3) + l
+    in case compare s (fromIntegral y3) of
+        LT    -> Right
+        GT    -> Left
+        EQ    -> Straight
+
 
 test_Left =
     direction (Point2D {x=0, y=0}) (Point2D {x=1, y=1}) (Point2D {x=2, y=3})
