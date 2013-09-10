1a30fe017ce36c87ea86c9bfb02a747be26298ea
Michael Pankov <constantius9@gmail.com>
2013-09-11 00:42:54 +0400

    Improve palindrome detector

--- ch03/Exercises.hs
+++ ch03/Exercises.hs
@@ -15,15 +15,8 @@ palindrome :: String -> String
 palindrome xs = xs ++ (reverse xs)
 
 isPalindrome :: String -> Bool
-isPalindrome []    = True
-isPalindrome [x]   = True
-isPalindrome [x,y] = x == y
-isPalindrome xs    =
-	let h = head xs
-	    l = last xs
-	    i = init xs
-	    m = tail i
-	in h == l && (isPalindrome m)
+isPalindrome xs =
+	all (uncurry (==)) $ zip xs (reverse xs)
 
 byLength :: [a] -> [a] -> Ordering
 byLength xs1 xs2 = compare (length xs1) (length xs2)
