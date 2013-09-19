ccc722914e85b70102a08dc40daf4af2a140965b
Michael Pankov <constantius9@gmail.com>
2013-09-20 01:49:46 +0400

    Add ugly split implementation

--- ch04/Exercises.hs
+++ ch04/Exercises.hs
@@ -14,4 +14,19 @@ safeLast (_:t) = safeLast t
 safeInit :: [a] -> Maybe [a]
 safeInit [] = Nothing
 safeInit (h:a:[]) = Just [h]
-safeInit (h:t) = fmap ([h] ++ ) (safeInit t)
+safeInit (h:t) = fmap (h : ) (safeInit t)
+
+splitWith p l =
+	reverse $ accumulate [] [] p l
+
+accumulate a1 a2 p [] =
+	if a2 /= []
+		then reverse a2 : a1
+		else a1
+accumulate a1 a2 p (h:t) =
+	if p h
+		then accumulate a1 (h:a2) p t
+		else
+			if a2 /= []
+				then accumulate (reverse a2 : a1) [] p t
+				else accumulate a1 [] p t
