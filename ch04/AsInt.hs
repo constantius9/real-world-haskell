ed9904993c528958983939dd5d231c4b4ec3088a
Michael Pankov <constantius9@gmail.com>
2013-09-22 22:14:58 +0400

    Add error handling via Either

--- ch04/AsInt.hs
+++ ch04/AsInt.hs
@@ -10,38 +10,52 @@ import Test.HUnit
 isDigit c =
     '0' <= c && c <= '9'
 
-convertAdd acc x@(c:_) =
+sign x
+    | x >= 0    = ( 1)
+    | otherwise = (-1)
+
+isNotOverflow x x' =
+    sign x == sign x'
+
+convertAdd (Left  errorMessage) _       = Left errorMessage
+convertAdd (Right acc)          x@(c:_) =
     if isDigit c
         then
             let digit = (read x :: Int)
-            in  acc * 10 + digit
-        else error $ "convertAdd: not a digit " ++ x
+                nacc  = acc * 10 + digit
+            in  if  isNotOverflow nacc acc
+                then Right nacc
+                else Left $ "convertAdd: integer overflow: old acc == "
+                          ++ show acc ++ " newacc == " ++ show nacc
+        else Left $ "convertAdd: not a digit " ++ x
 
 breakAll [] = []
 breakAll (h:t) =
     [h] : breakAll t
 
-asInt_fold :: String -> Int
-asInt_fold "" = 0
+type ErrorMessage = String
+
+asInt_fold :: String -> Either ErrorMessage Int
+asInt_fold "" = Left "empty string to convert"
 asInt_fold s@(h:t)
-    | h == '-'  = (-1) * (foldl convertAdd 0 $ breakAll t)
-    | otherwise = ( 1) * (foldl convertAdd 0 $ breakAll s)
+    | h == '-'  = fmap ( * (-1) ) (foldl convertAdd (Right 0) $ breakAll t)
+    | otherwise = fmap ( * ( 1) ) (foldl convertAdd (Right 0) $ breakAll s)
 
 test_AsInt1 =
     asInt_fold "101"
-    @?= 101
+    @?= Right 101
 
 test_AsInt2 =
     asInt_fold "-31337"
-    @?= (-31337)
+    @?= Right (-31337)
 
 test_AsInt3 =
     asInt_fold "1798"
-    @?= 1798
+    @?= Right 1798
 
 test_AsInt4 =
     asInt_fold "200"
-    @?= 200
+    @?= Right 200
 
 main = defaultMain tests
 
