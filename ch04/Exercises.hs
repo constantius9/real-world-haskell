2d8ca46d91a45acffce0a325cfdee907598b3b0e
Michael Pankov <constantius9@gmail.com>
2013-09-24 00:39:16 +0400

    Implement groupBy

--- ch04/Exercises.hs
+++ ch04/Exercises.hs
@@ -1,3 +1,5 @@
+import Prelude hiding (concat, takeWhile, groupBy)
+
 safeHead :: [a] -> Maybe a
 safeHead [] = Nothing
 safeHead (h:_) = Just h
@@ -30,3 +32,37 @@ accumulate a1 a2 p (h:t) =
 			if a2 /= []
 				then accumulate (reverse a2 : a1) [] p t
 				else accumulate a1 [] p t
+
+concat1 :: [a] -> [a] -> [a]
+concat1 []    acc = acc
+concat1 (h:t) acc =
+	h : concat1 t acc
+
+concat :: [[a]] -> [a]
+concat l = foldr concat1 [] l
+
+takeWhile :: (a -> Bool) -> [a] -> [a]
+takeWhile p [] = []
+takeWhile p (h:t)
+	| p h = h : takeWhile p t
+	| otherwise = []
+
+takeWhile1 p h acc
+	| p h = h : acc
+	| otherwise = acc
+
+takeWhileF :: (a -> Bool) -> [a] -> [a]
+takeWhileF p l = foldr (takeWhile1 p) [] l
+
+groupBy1 :: (a -> a -> Bool) -> a -> [[a]] -> [[a]]
+groupBy1 _ h [] =
+	[[h]]
+
+groupBy1 p h acc
+	| p h h1 = (h : ha) : acc
+	| otherwise = [h] : acc
+	where ha = head acc
+	      h1 = head ha
+
+groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy p l = foldr (groupBy1 p) [] l
